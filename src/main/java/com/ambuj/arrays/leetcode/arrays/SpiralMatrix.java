6e881980c44766785d1c9fb90f807277961b2d4c
ambuj <ambujjauhari@yahoo.co.in>
2021-05-31 19:29:48 +0530

    SpiralMatrix : optimize solution a bit

--- src/main/java/com/ambuj/arrays/leetcode/arrays/SpiralMatrix.java
+++ src/main/java/com/ambuj/arrays/leetcode/arrays/SpiralMatrix.java
@@ -4,82 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralMatrix {
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public List<Integer> spiral(int[][] matrix) {
         List<Integer> integers = new ArrayList<Integer>();
         int topRowIdx = 0;
-        int rightRowIdx = matrix[0].length;
+        int rightColumnIdx = matrix[0].length;
         int bottomRowIdx = matrix.length;
-        int leftRowIdx = 0;
-        int i = topRowIdx;
-        int j = topRowIdx;
+        int leftColumnIdx = 0;
+        int i = 0;
 
-        while (i < bottomRowIdx && j < rightRowIdx) {
-            if(topRowIdx == bottomRowIdx-1 && leftRowIdx == rightRowIdx-1) {
-                integers.add(matrix[topRowIdx][leftRowIdx]);
-                break;
+        //loop all the elements of boundry
+        while (topRowIdx < bottomRowIdx && leftColumnIdx < rightColumnIdx) {
+            //print first row
+            for (i = leftColumnIdx; i < rightColumnIdx; i++) {
+                System.out.println(matrix[topRowIdx][i]);
             }
-            //top row
-            if (i == topRowIdx) {
-                if (j < rightRowIdx) {
-                    integers.add(matrix[i][j]);
-                    j++;
-                }
-                if (j == rightRowIdx - 1) {
-                    integers.add(matrix[i][j]);
-                    i++;
-                }
-            }
-            //right row
-            if (j == rightRowIdx - 1) {
-                if (i < bottomRowIdx) {
-                    integers.add(matrix[i][j]);
-                    i++;
-                }
-                if (i == bottomRowIdx - 1) {
-                    integers.add(matrix[i][j]);
-                    j--;
-                }
-            }
-//                //bottom row
-            if (i == bottomRowIdx - 1 && topRowIdx != bottomRowIdx-1) {
-                if (j > leftRowIdx) {
-                    integers.add(matrix[i][j]);
-                    j--;
-                }
-                if (j == leftRowIdx) {
-                    integers.add(matrix[i][j]);
-                    i--;
-                }
-            }
-//                //left row
-            if (j == leftRowIdx && leftRowIdx != rightRowIdx-1) {
-                if (i > topRowIdx) {
-                    integers.add(matrix[i][j]);
-                    i--;
-                }
-            }
+            //increment first row
+            topRowIdx++;
 
-            if (i == topRowIdx && j == topRowIdx) {
-                topRowIdx++;
-                rightRowIdx--;
+            //print right column
+            for (i = topRowIdx; i < bottomRowIdx; i++) {
+                System.out.println(matrix[i][rightColumnIdx - 1]);
+            }
+            rightColumnIdx--;
+
+            //print bottom row
+            if (topRowIdx < bottomRowIdx) {
+                for (i = rightColumnIdx - 1; i >= leftColumnIdx; i--) {
+                    System.out.println(matrix[bottomRowIdx - 1][i]);
+                }
                 bottomRowIdx--;
-                leftRowIdx++;
-                i = topRowIdx;
-                j = topRowIdx;
+            }
+
+            //print left row
+            if (leftColumnIdx < rightColumnIdx) {
+                for (i = bottomRowIdx - 1; i >= topRowIdx; i--) {
+                    System.out.println(matrix[i][leftColumnIdx]);
+                }
+                leftColumnIdx++;
             }
         }
-
-
         return integers;
+    }
+
+    public static boolean areAllEqual(int checkValue, int... otherValues) {
+        for (int value : otherValues) {
+            if (value != checkValue) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String... args) {
         int[][] array = new int[][]{
-                {1,2,3,4},
-                {5,6,7,8},
-                {9,10,11,12}
+                {1, 2, 3, 4, 5},
+                {16, 17, 18, 19, 6},
+                {15, 24, 25, 20, 7},
+                {14, 23, 22, 21, 8},
+                {13, 12, 11, 10, 9}
         };
-        System.out.println(new SpiralMatrix().spiralOrder(array));
+        System.out.println(new SpiralMatrix().spiral(array));
     }
 
 }
